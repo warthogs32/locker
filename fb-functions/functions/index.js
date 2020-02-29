a07a8b26f10730bcea8156d5de9aeeb1414eef19
etnom <montgomery.okdaily@gmail.com>
2020-02-29 09:17:07 -0800

    Test function to test facial detection

--- fb-functions/functions/index.js
+++ fb-functions/functions/index.js
@@ -1,11 +1,31 @@
 const functions = require('firebase-functions');
+const vision = require('@google-cloud/vision');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const faceDetectionTest = async function () {
+  const client = new vision.ImageAnnotatorClient();
+
+  const fileName = './2uglies.jpg'; 
+
+  // const base64Img = ""
+  
+  const [result] = await client.faceDetection(fileName);
+
+  console.log(result)
+
+  const faces = result.faceAnnotations;
+  console.log('Faces:');
+  faces.forEach((face, i) => {
+    console.log(`  Face #${i + 1}:`);
+    console.log(`    Joy: ${face.joyLikelihood}`);
+    console.log(`    Anger: ${face.angerLikelihood}`);
+    console.log(`    Sorrow: ${face.sorrowLikelihood}`);
+    console.log(`    Surprise: ${face.surpriseLikelihood}`);
+  });
+
+}
+
+faceDetectionTest()
 
 
 exports.helloWorld = functions.https.onRequest((req, res) => {
