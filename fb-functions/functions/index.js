82d3a1a2b148c1e0f697ab2577f3a5f4adb90b8e
etnom <montgomery.okdaily@gmail.com>
2020-02-29 13:30:12 -0800

    Attempt at cloud function that subscribes to firestore

--- fb-functions/functions/index.js
+++ fb-functions/functions/index.js
@@ -41,7 +41,8 @@ const imageDataCollectionRef = db.collection('image-data');
 
 
 const getFacesFromImage = async function (base64Img) {
-  const [ result ] = await imageDetectionclient.faceDetection(Buffer.from(base64Img, 'base64'));
+  const [ result ] = await imageDetectionclient
+    .faceDetection(Buffer.from(base64Img, 'base64'));
 
   const faces = result.faceAnnotations;
   console.log('Faces:');
@@ -63,42 +64,33 @@ let rtObserver = imageDataCollectionRef.onSnapshot(snapshot => {
     // Grab current image that's base 64 encoded 
     const currentImage = docData.currentImage;
 
-    console.log(currentImage);
-
     getFacesFromImage(currentImage)
   })
 })
 
-const faceDetectionTest = async function () {
-  const client = new vision.ImageAnnotatorClient();
 
-  const fileName = './test_images/2uglies.jpg'; 
+myFunction(a, b, 
+  function () {
+    
+  });
 
-  imageToBase64(fileName)
-    .then(async (base64Img) => {
-      const [ result ] = await client.faceDetection(Buffer.from(base64Img, 'base64'));
 
-      const faces = result.faceAnnotations;
-      console.log('Faces:');
-      faces.forEach((face, i) => {
-        console.log(`  Face #${i + 1}:`);
-        console.log(`    Joy: ${face.joyLikelihood}`);
-        console.log(`    Anger: ${face.angerLikelihood}`);
-        console.log(`    Sorrow: ${face.sorrowLikelihood}`);
-        console.log(`    Surprise: ${face.surpriseLikelihood}`);
-      });
-    });
-  
 
-}
 
-// faceDetectionTest()
 
 // Test helloWorld cloud function that triggers on http request
 exports.helloWorld = functions.https.onRequest((req, res) => {
   res.send("Goodbye from Firebase!");
+  console.log("hello")
 });
 
 // Cloud function to run facial detection processes on images
 // Trigger on firebase image collection update
 // exports.facialDetection = functions.
+exports.myFunction = functions.firestore
+  .document('image-data')
+  .onWrite((change, context) => {
+    console.log("change: ", change);
+  }); 
+
+
