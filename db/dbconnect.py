eba9547c2e61f544914fdfb010906f9aaf332688
jason.jen <jj@jj-jj.org>
2020-02-29 02:27:43 -0800

    test db connect

--- db/dbconnect.py
+++ db/dbconnect.py
@@ -5,7 +5,7 @@ from firebase_admin import firestore
 cred = credentials.Certificate('slohacks-269509-firebase-adminsdk-dun4x-9342e5bf13.json')
 firebase_admin.initialize_app(cred)
 
-db = firestore.client()
+db = firestore.Client()
 doc_ref = db.collection(u'log').document(u'test')
 doc_ref.set(
 {
