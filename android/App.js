bec7c3e7d5b8b6e4ae4adbcf8afdf27dee2c7cf2
Ben Clark <bclark33@calpoly.edu>
2020-02-29 04:19:11 -0800

    moved project into seperate android directory

---
+++ android/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  return (
+    <View style={styles.container}>
+      <Text>please please please work</Text>
+    </View>
+  );
+}
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
