b50bf91268f9d035d0be199cb99c06da8c7c5fca
etnom <montgomery.okdaily@gmail.com>
2020-02-29 10:30:19 -0800

    Connect to firebase realtime db

--- fb-functions/functions/index.js
+++ fb-functions/functions/index.js
@@ -1,12 +1,26 @@
 const functions = require('firebase-functions');
 const vision = require('@google-cloud/vision');
 const imageToBase64 = require('image-to-base64');
+const admin = require('firebase-admin');
 
+
+
+let serviceAccount = require("./service-account-file.json");
+
+let app = admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://slohacks-269509.firebaseio.com"
+});
+
+let db = app.database();
+
+
+console.log(db)
 
 const faceDetectionTest = async function () {
   const client = new vision.ImageAnnotatorClient();
 
-  const fileName = './2uglies.jpg'; 
+  const fileName = './test_images/2uglies.jpg'; 
 
   imageToBase64(fileName)
     .then(async (base64Img) => {
@@ -28,7 +42,11 @@ const faceDetectionTest = async function () {
 
 faceDetectionTest()
 
-
+// Test helloWorld cloud function that triggers on http request
 exports.helloWorld = functions.https.onRequest((req, res) => {
   res.send("Goodbye from Firebase!");
 });
+
+// Cloud function to run facial detection processes on images
+// Trigger on firebase image collection update
+// exports.facialDetection = functions.
