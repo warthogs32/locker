d014bcb0fe777ac3a0b786dfa77e7b0915e9c05a
etnom <montgomery.okdaily@gmail.com>
2020-02-29 09:21:37 -0800

    implement imageToBase64 to base 64 encode the image and send to google api

--- fb-functions/functions/index.js
+++ fb-functions/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const vision = require('@google-cloud/vision');
+const imageToBase64 = require('image-to-base64');
 
 
 const faceDetectionTest = async function () {
@@ -7,21 +8,24 @@ const faceDetectionTest = async function () {
 
   const fileName = './2uglies.jpg'; 
 
-  // const base64Img = ""
-  
-  const [result] = await client.faceDetection(fileName);
 
-  console.log(result)
+  imageToBase64(fileName)
+  .then(async (response) => {
+    const [result] = await client.faceDetection(fileName);
 
-  const faces = result.faceAnnotations;
-  console.log('Faces:');
-  faces.forEach((face, i) => {
-    console.log(`  Face #${i + 1}:`);
-    console.log(`    Joy: ${face.joyLikelihood}`);
-    console.log(`    Anger: ${face.angerLikelihood}`);
-    console.log(`    Sorrow: ${face.sorrowLikelihood}`);
-    console.log(`    Surprise: ${face.surpriseLikelihood}`);
+    const faces = result.faceAnnotations;
+    console.log('Faces:');
+    faces.forEach((face, i) => {
+      console.log(`  Face #${i + 1}:`);
+      console.log(`    Joy: ${face.joyLikelihood}`);
+      console.log(`    Anger: ${face.angerLikelihood}`);
+      console.log(`    Sorrow: ${face.sorrowLikelihood}`);
+      console.log(`    Surprise: ${face.surpriseLikelihood}`);
+    });
   });
+
+  
+  
 
 }
 
