3e713104f557d38fb3f1fc575841a90f805ab216
etnom <montgomery.okdaily@gmail.com>
2020-02-29 00:54:28 -0800

    Add navbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './Components/App/App';
 import * as serviceWorker from './serviceWorker';
 
+import './styles/containers.css'
+import './styles/misc.css'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
