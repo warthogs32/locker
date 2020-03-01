878dfb7ea1cc080019c5c861fa68052ed22f2479
etnom <montgomery.okdaily@gmail.com>
2020-02-29 20:12:28 -0800

    Subscribe state to rtdb

--- frontend/src/Components/App/App.js
+++ frontend/src/Components/App/App.js
@@ -1,13 +1,46 @@
-import React from 'react';
-
-
+import React, { useState, useEffect } from 'react';
+import * as firebase from 'firebase';
 
 import Navbar from '../Navbar';
 
+import { useList } from 'react-firebase-hooks/database';
 
 import './App.css';
 
-function App() {
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXHTIuyXFoHfzDM0nkRmkVMEa2B2H8hxY",
+  authDomain: "slohacks-269509.firebaseapp.com",
+  databaseURL: "https://slohacks-269509.firebaseio.com",
+  projectId: "slohacks-269509",
+  storageBucket: "slohacks-269509.appspot.com",
+  messagingSenderId: "622446993760",
+  appId: "1:622446993760:web:8d97fad7d5536ffeec3e05",
+  measurementId: "G-79BT5R74K9"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+const App = props => {
+  const [ locationData, setLocationData ] = useState({});
+  const [ imageData, setImageData ] = useState({});
+
+  // Subscribe state to rtdb on component mount
+  useEffect(() => {
+    const db = firebase.database();
+    const dbRef = db.ref();
+
+    dbRef.on('value', snapshot => {
+      setLocationData(snapshot.val().geo_data);
+      setImageData(snapshot.val().image_data);
+    })
+  }, [])
+
+  console.log(locationData)
+  console.log(imageData)
+
 
   return (
     <div className="App">
