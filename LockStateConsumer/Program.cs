deea5856e32dcb28abdb9707fb487a1e7bb8bb7e
Jason Jen <jj@jj-jj.org>
2020-02-29 17:18:50 -0800

    #4 firebase client for lock/unlock work in progress

---
+++ LockStateConsumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Resources;
+using System.Threading.Tasks;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+namespace LockStateConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string auth = Properties.Resources.secret;
+            var firebaseClient = new FirebaseClient(Properties.Resources.url, new FirebaseOptions
+            {
+                AuthTokenAsyncFactory = () => Task.FromResult(auth)
+            });
+
+            while (true)
+            {
+                var locks = firebaseClient.Child("lockstate").AsObservable<LockStateEnum>().Subscribe(s => Console.WriteLine(s));
+            }
+        }
+    }
+}
