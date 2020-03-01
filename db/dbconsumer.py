e9e7d870d32da0433b56f261f63a0c3d9c6fc797
jason.jen <jj@jj-jj.org>
2020-02-29 16:44:24 -0800

    rewrite in c#

--- db/dbconsumer.py
+++
@@ -1,8 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
-cred = credentials.Certificate('slohacks-269509-firebase-adminsdk-dun4x-9342e5bf13.json')
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
